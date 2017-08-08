c5480131024fe5b002582399428f8b8187af2763
Tyler Shambora <tyler@bvaccel.com>
2017-08-08 11:14:41 -0700

    updates messaging again

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const createChangeTable = data => {
   const tableStyle = 'style="border-collapse: collapse; border: solid #e0e0dc; border-width: 1px 0 0 1px; width: 100%;"'
   const headCellStyle = 'style="border: solid #e0e0dc; border-width: 0 1px 1px 0; padding: 6px 8px; text-align: left;background: rgba(212,221,228,.5);"'
   const cellStyle = 'style="border: solid #e0e0dc; border-width: 0 1px 1px 0; padding: 6px 8px; text-align: left;"'
-  const tableHead = `<thead><tr><th ${headCellStyle}>Repo</th><th ${headCellStyle}>Then (${then})</th><th ${headCellStyle}>Now (${now})</th><th ${headCellStyle}>Change</th></tr></thead>`;
+  const tableHead = `<thead><tr><th ${headCellStyle}>Repo</th><th ${headCellStyle}>Then</th><th ${headCellStyle}>Now</th><th ${headCellStyle}>Change</th></tr></thead>`;
 
   if (data.length > 0) {
     tableBody = data.reduce((string, item) => {
@@ -79,7 +79,7 @@ const createChangeTable = data => {
 };
 
 const createEmailString = table => {
-  return `<div style="width: 600px; margin: 0 auto;"><h2 style="font-size: 34px;">Howdy!</h2>Just what you were hoping for! Another email! In this email we'll take a look at recent changes in our project's CodeClimate GPAs and call out the ones with big movements. I bet you thought you were done getting grades and report cards after you finished school! Well, think again!<br><br>A few notes: The CodeClimate API is still in beta so it's liiiiiittle unpredictable and undocumented. As such, I can't guarantee the accuracy or consistency of this email, but hopefully one day it grows into a beautiful <a href="https://www.youtube.com/watch?v=PDBBCuw_Rpc">delicate little flower.</a><br><br><h2 style="font-size: 34px;"><u>Movers and Shakers</u></h2>Here are the big changes from the last week:<br><br>${table}<br><br>---<br><br>Boy, wasn't that some good data?!?? Be on the lookout for this same email next week!<br><br>Until then, stay sexy and don't get murdered yall!</div>`;
+  return `<div style="width: 600px; margin: 0 auto;"><h2 style="font-size: 34px;">Howdy!</h2>Just what you were hoping for! Another email! In this email we'll take a look at recent changes in our project's CodeClimate GPAs and call out the ones with big movements. I bet you thought you were done getting grades and report cards! Well, think again!<br><br><em>A few notes: The CodeClimate API is still in beta so it's liiiiiittle unpredictable and undocumented. As such, I can't guarantee the accuracy or consistency of this email, but hopefully one day it grows into a beautiful <a href="https://youtu.be/PDBBCuw_Rpc">delicate little flower.</a></em><br><h2 style="font-size: 34px;"><u>Movers and Shakers</u></h2>Here are the big changes between ${then} and ${now}:<br><br>${table}<br><br>---<br><br>Boy, wasn't that some good data?!?? Be on the lookout for this same email next week!<br><br>Until then, stay sexy and don't get murdered yall!</div>`;
 };
 
 const sendEmail = emailString => {
